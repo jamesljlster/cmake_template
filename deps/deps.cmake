1243d01d31b4d2542775f9cf91f572c22ee23889
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-11-12 00:37:10 +0800

    Checkout files with 'revert' branch

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -1,20 +1,23 @@
 # Set dependences paths
-set(DEPS_PATHS)
+set(DEPS_PATHS
+	${CMAKE_CURRENT_SOURCE_DIR}/deps/deps1
+	${CMAKE_CURRENT_SOURCE_DIR}/deps/deps2
+	)
 include_directories(${DEPS_PATHS})
 
 # Find other dependences
 find_package(OpenCV REQUIRED)
 include_directories(${OpenCV_INCLUDE_DIRS})
 
-#find_path(SERIALPORT_INCLUDE_DIR libserialport.h
-#	"/usr/include"
-#	"/usr/local/include"
-#	)
-#find_library(SERIALPORT_LIB libserialport.a
-#	"/usr/lib"
-#	"/usr/local/lib"
-#	)
-#include_directories(${SERIALPORT_INCLUDE_DIR})
+find_path(SERIALPORT_INCLUDE_DIR libserialport.h
+	"/usr/include"
+	"/usr/local/include"
+	)
+find_library(SERIALPORT_LIB libserialport.a
+	"/usr/lib"
+	"/usr/local/lib"
+	)
+include_directories(${SERIALPORT_INCLUDE_DIR})
 
 # Add subdirectory
 foreach(DEPS_PATH ${DEPS_PATHS})
