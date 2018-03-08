a717c98ac2d453d7d2a60bba467cd6d44f1f5577
Yu-Ren, Zhang <e8035669@gmail.com>
2018-03-09 00:41:05 +0800

    Try to use find modules

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -12,14 +12,9 @@ set(OpenCV_STATIC OFF CACHE BOOL "Using OpenCV static linking library")
 find_package(OpenCV REQUIRED)
 include_directories(${OpenCV_INCLUDE_DIRS})
 
-find_path(SERIALPORT_INCLUDE_DIR libserialport.h
-	"/usr/include"
-	"/usr/local/include"
-	)
-find_library(SERIALPORT_LIB libserialport.a
-	"/usr/lib"
-	"/usr/local/lib"
-	)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+
+find_package(Serialport REQUIRED)
 include_directories(${SERIALPORT_INCLUDE_DIR})
 
 # Add subdirectory
